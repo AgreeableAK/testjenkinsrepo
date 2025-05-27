df1ab2febf071eb30feabad8017f69f2b25fb9d1
AgreeableAK <anujsehajwal@gmail.com>
2025-05-28 00:41:47 +0530

    hdsfhdaslkhf

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,16 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {	
+	int a=5;
+	int b=6;
+	int c=a+b;
+        System.out.println( "Hello World! a+b = "+ c );
+    }
+}
